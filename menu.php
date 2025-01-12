732cdbe28efb518de4c0f4e2658f3e3c4c710c32
전우혁 <lamp030225@gmail.com>
2025-01-12 16:54:51 +0900

    수정된 코드 반영

--- menu.php
+++ menu.php
@@ -8,77 +8,41 @@
 ?>
 
 <header>
-  <!-- Fixed navbar -->
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/class2/bookmarket/welcome.php">                 
+            <a class="navbar-brand" href="welcome.php">
                 <i class="fa-solid fa-book-open"></i>
-                <span class="fs-5">BookMarket</span> 
+                <span class="fs-5">BookMarket</span>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">            
-                
-            <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/class2/bookmarket/welcome.php">Home</a>
-                </li>                      
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/class2/bookmarket/books.php">도서목록</a>
-                </li> 
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/class2/bookmarket/addBook.php">도서등록</a>
-                </li> 
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/class2/bookmarket/editsBooks.php?edit=update">도서수정</a>
-                </li> 
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/class2/bookmarket/editsBooks.php?edit=delete">도서삭제</a>
-                </li> 
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/class2/bookmarket/cart.php">장바구니</a>
-                </li> 
-            </ul>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="welcome.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="books.php">도서목록</a></li>
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="addBook.php">도서등록</a></li>
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="editsBooks.php?edit=update">도서수정</a></li>
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="editsBooks.php?edit=delete">도서삭제</a></li>
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="cart.php">장바구니</a></li>
+                </ul>
 
-            <ul class="navbar-nav me-right mb-2 mb-md-0">
-                <?php
-                    if (empty($sessionID) || $sessionID == null) {
-                ?>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page"
-                        href="/class2/bookmarket/member/loginMember.php">로그인</a>
-                    </li>
+                <ul class="navbar-nav me-right mb-2 mb-md-0">
+                    <?php if (empty($sessionID)) { ?>
+                        <li class="nav-item"><a class="nav-link active" href="member/loginMember.php">로그인</a></li>
+                        <li class="nav-item"><a class="nav-link active" href="member/addMember.php">회원가입</a></li>
+                    <?php } else { ?>
+                        <li class="nav-link active" style="padding-top: 7px; color:green"><b>[<?=$sessionID;?>]님</b></li>
+                        <li class="nav-item"><a class="nav-link active" href="member/logoutMember.php">로그아웃</a></li>
+                        <li class="nav-item"><a class="nav-link active" href="member/updateMember.php">회원수정</a></li>
+                    <?php } ?>
+                </ul>
 
-                    <li class="nav-item">    
-                        <a class="nav-link active" aria-current="page"
-                        href="/class2/bookmarket/member/addMember.php">회원가입</a>
-                    </li>
-                <?php
-                    } else { //로그인 상태
-                ?>
-                <li class="nav-link active" style="padding-top: 7px; color:green">
-                    <b>[<?=$sessionID;?>]님</b>
-                </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page"
-                        href="/class2/bookmarket/member/logoutMember.php">로그아웃</a>
-                    </li>
-
-                    <li class="nav-item">    
-                        <a class="nav-link active" aria-current="page"
-                        href="/class2/bookmarket/member/updateMember.php">회원수정</a>
-                    </li>
-                    <?php
-                        }
-                    ?>
-            </ul>
-
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>               
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
             </div>
-         </div>
+        </div>
     </nav>
 </header>
